412e54af666c6e3b9ed3646d046439d332a712e0
breadcat-dev <breadcat.dev@proton.me>
2026-07-10 12:33:47 +0300

    refactor: change EPSILON to 1e-9 - change EPSILON in MathConstants from 1e-12 to 1e-9

--- src/main/java/cat/breadcat/toolbox/constant/MathConstants.java
+++ src/main/java/cat/breadcat/toolbox/constant/MathConstants.java
@@ -4,6 +4,6 @@ public final class MathConstants
 {
     private MathConstants() {}
 
-    public static final double EPSILON = 1e-12;
+    public static final double EPSILON = 1e-9;
     public static final float EPSILON_F = 1e-6f;
 }
