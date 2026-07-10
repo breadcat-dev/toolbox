b6607d1ffe277ba17a649569a01be104d3266003
breadcat-dev <breadcat.dev@proton.me>
2026-07-10 12:38:34 +0300

    feat: add STRICT_EPSILON - add STRICT_EPSILON (1e-12) and STRICT_EPSILON(1e-6) in MathConstants - refactor EPSILON_F from 1e-6f to 1e-5f

--- src/main/java/cat/breadcat/toolbox/constant/MathConstants.java
+++ src/main/java/cat/breadcat/toolbox/constant/MathConstants.java
@@ -5,5 +5,7 @@ public final class MathConstants
     private MathConstants() {}
 
     public static final double EPSILON = 1e-9;
-    public static final float EPSILON_F = 1e-6f;
+    public static final double STRICT_EPSILON = 1e-12;
+    public static final float EPSILON_F = 1e-5f;
+    public static final float STRICT_EPSILON_F = 1e-6f;
 }
